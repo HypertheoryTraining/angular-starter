29ab76cf5aa8da462a00e7d4b83bdbf09704882e
Jeffry <jeff@hypertheory.com>
2024-11-14 13:51:15 -0500

    added alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { WelcomeComponent } from './components/welcome.component';
 
 @Component({
-    selector: 'app-root',
-    template: `
+  selector: 'app-root',
+  template: `
     <main class="container mx-auto">
       <app-welcome />
     </main>
   `,
-    styles: [],
-    imports: [ WelcomeComponent]
+  styles: [],
+  imports: [WelcomeComponent],
 })
 export class AppComponent {}
