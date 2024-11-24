8a7e99b4c20d2046305b5499a142912ac08a8ca1
Jeff Gonzalez <Jeff@hypertheory.com>
2024-11-24 18:40:19 -0500

    changed welcome

--- src/app/components/welcome.component.ts
+++ src/app/components/welcome.component.ts
@@ -1,26 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-    selector: 'app-welcome',
-    imports: [],
-    template: `
+  selector: 'app-welcome',
+  imports: [],
+  template: `
     <div class="prose pt-12">
       <h1>Angular Starter</h1>
-      <p>Using Angular 19.0.0-rc.2</p>
-      <div role="alert" class="alert alert-success">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6 shrink-0 stroke-current"
-          fill="none"
-          viewBox="0 0 24 24">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-        <span>Angular 19 is set to be released on November 19th, 2024. We will do an update during class when it releases.</span>
-      </div>
+      <p>Using Angular {{ v.full }}</p>
+
       <ul>
         <li><a href="https://tailwindcss.com/">Tailwind for CSS</a></li>
         <li><a href="https://daisyui.com/">DaisyUi for UI Library</a></li>
@@ -30,6 +17,8 @@ import { Component } from '@angular/core';
       </ul>
     </div>
   `,
-    styles: ``
+  styles: ``,
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  v = VERSION;
+}
